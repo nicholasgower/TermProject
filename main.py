1b21e9d3aee694673ccc124b458d88a6cbaac7fb
nicholasgower <nicholasgower01@gmail.com>
2020-03-11 12:49:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,31 @@ from pylatex.table import Tabular
 import matplotlib
 from pylatex.position import Center
 import matplotlib.pyplot as plt
+
+authors=["Nicholas Gower","Caleb Puls","Gauthan Anant","Ethan Radford"]
+sections=['Title', 'Personnel', 'Executive Summary', 'Introduction', 'Equipment',"Data and analysis","Discussion and conclusion"]
+for name in sections:
+    open(name+".txt",'w').close()
+def addSection(document,title,fileName):
+  with doc.create(tex.Section(title):
+    doc.append(readText(fileName))
+def main():
+	pickleData("data.xlsx","data.labdata")
+	pickleData("equipment.xlsx","equipment.labdata")
+	data=loadData("data.labData")
+	equipmentList=loadData("equipment.labdata")
+				  
+				  
+	doc=tex.Document("book",geometry_options={"margin":"1in")
+  doc.preamble.append(Command('author', ",".join(authors))
+  doc.preamble.append(Command('title',"Constructing a Tesla Coil")
+  doc.append(Command("maketitle"))
+  addSection(doc,'Introduction','Introduction.txt')
+  with doc.create(tex.Section("Equipment"):
+    with doc.create(Tabular("| l | l | l | l | l |")) as table:
+      table.add_hline()
+      table.add_row(["Name","SKU","Vendor","Price","Shipping Time"])
+      table.add_hline()    
+      for row in sheet:
+      	table.add_row([row['Name'],row["SKU"],row["Vendor"],row["Price"],row["Shipping Time"])
+      	table.add_hline()
